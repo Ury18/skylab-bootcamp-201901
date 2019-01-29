e5e1a660e2e6c3bf7a28cb7716781099b147d548
Ury18 <ury_98@hotmail.com>
2019-01-29 12:23:57 +0100

    register and login to React

--- staff/oriol-escolar/spotify-app/react/scripts/main.jsx
+++ staff/oriol-escolar/spotify-app/react/scripts/main.jsx
@@ -1,3 +1,96 @@
+
+function Feedback({ message, level }) {
+    return <section className={`feedback ${level ? `feedback--${level}` : ''}`}>{message}</section>
+}
+
+
+
+class App extends React.Component {
+
+    state = {
+
+        loginFeedback: '',
+        registerFeedback:'',
+        loginVisible:true,
+        registerVisible: false
+
+    }
+
+    handleLogin = (email, password) => {
+        try {
+            logic.login(email, password, user => {
+                console.log(user)
+
+                this.setState({ loginFeedback: '' })
+            })
+        } catch ({ message }) {
+            this.setState({ loginFeedback: message })
+        }
+    }
+
+
+    handleRegister = (name, surname, email, password, passwordConfirmation) => {
+
+        
+        try {
+            
+            logic.register(name, surname, email, password, passwordConfirmation, () => {
+                this.setState({registerFeedback:''})
+                this.goToLogin();
+
+            })
+
+
+        } catch ({ message }) {
+
+            this.setState({registerFeedback:message})
+
+        }
+
+
+    }
+
+
+    goToRegister = ()=>{
+
+        console.log(this.state)
+        this.setState({loginVisible:false})
+        this.setState({registerVisible:true})
+
+
+    }
+    goToLogin= () =>{
+
+        this.setState({loginVisible:true})
+        this.setState({registerVisible:false})
+
+
+    }
+
+
+    render() {
+
+        const { state: { loginFeedback,registerFeedback,loginVisible,registerVisible }, handleLogin, handleRegister,goToLogin,goToRegister } = this
+
+        return <main className='app'>
+            <header class="text-center">
+                <h1>🎶 Spotify App 🎧</h1>
+            </header>
+            
+            
+            {loginVisible && <LoginPanel onLogin={handleLogin} feedback={loginFeedback} goToRegister={goToRegister} />}
+            {registerVisible && <RegisterPanel onRegister={handleRegister} feedback = {registerFeedback} goToLogin ={goToLogin}/>}
+
+            
+            
+
+
+        </main>
+    }
+
+
+}
+
 class LoginPanel extends React.Component {
 
     state = {
@@ -17,11 +110,19 @@ class LoginPanel extends React.Component {
 
     }
 
+    handleForButton = event => {
+        event.preventDefault();
+        const {props: {goToRegister}}=this
+        goToRegister();
+    }
+
+    
+
 
 
     render() {
 
-        const { handleEmailInput, handlePasswordInput, handleFormSubmit, props: { feedback } } = this
+        const { handleEmailInput, handlePasswordInput, handleFormSubmit,handleForButton, props: { feedback } } = this
 
         return <section className='login'>
             <h2>Login</h2>
@@ -33,6 +134,7 @@ class LoginPanel extends React.Component {
                 <button>Login</button>
             </form>
 
+            <button onClick={handleForButton}>Register</button>
 
             {feedback && <Feedback message={feedback} level='warn' />}
 
@@ -45,51 +147,70 @@ class LoginPanel extends React.Component {
 
 }
 
-function Feedback({ message, level }) {
-    return <section className={`feedback ${level ? `feedback--${level}` : ''}`}>{message}</section>
-}
-
-
-
-
-
-class App extends React.Component {
+class RegisterPanel extends React.Component {
 
     state = {
 
-        loginFeedback: ''
+        email: '',
+        password: '',
+        name: '',
+        surname: '',
+        confirmPassword: ''
 
     }
 
-    handleLogin = (email, password) => {
-        try {
-            logic.login(email, password, user => {
-                console.log(user)
+    handleEmailInput = event => this.setState({ email: event.target.value })
+    handlePasswordInput = event => this.setState({ password: event.target.value })
+    handleConfirmPasswordInput = event => this.setState({ confirmPassword: event.target.value })
+    handleNameInput = event => this.setState({ name: event.target.value })
+    handleSurnameInput = event => this.setState({ surname: event.target.value })
+    handleFormSubmit = event => {
+        event.preventDefault();
+        const { state: {name,surname, email, password, confirmPassword }, props: { onRegister } } = this;
+        onRegister(name,surname,email, password,confirmPassword)
 
-                this.setState({ loginFeedback: '' })
-            })
-        } catch ({ message }) {
-            this.setState({ loginFeedback: message })
-        }
+    }
+
+    handleForButton = event => {
+        event.preventDefault();
+        const {props: {goToLogin}}=this
+        goToLogin();
     }
 
 
     render() {
 
-        const { state: { loginFeedback }, handleLogin } = this
+        const { handleEmailInput, handlePasswordInput, handleConfirmPasswordInput, handleNameInput, handleSurnameInput, handleFormSubmit,handleForButton, props: { feedback } } = this
 
-        return <main className='app'>
-            <header class="text-center">
-                <h1>🎶 Spotify App 🎧</h1>
-            </header>
+        return <section className='register'>
+            <h2>Login</h2>
 
-            <LoginPanel onLogin={handleLogin} feedback={loginFeedback} />
+            <form onSubmit={handleFormSubmit}>
+                <label>Name</label>
+                <input type="text" onChange={handleNameInput} />
+                <label>surname</label>
+                <input type="text" onChange={handleSurnameInput} />
+                <label>Email</label>
+                <input type="email" onChange={handleEmailInput} />
+                <label>Password</label>
+                <input type="password" onChange={handlePasswordInput} />
+                <label>confirmPassword</label>
+                <input type="password" onChange={handleConfirmPasswordInput} />
+                <button>Register</button>
+            </form>
 
 
-        </main>
+            <button onClick={handleForButton}>LogIn</button>
+            {feedback && <Feedback message={feedback} level='warn' />}
+
+
+        </section>
+
+
     }
 
-
 }
+
+
 
 ReactDOM.render(<App />, document.getElementById('root'))   
